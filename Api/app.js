892c1b15ff468f75350a8a7e5e8f0f3828ace0c1
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-04-19 19:20:22 +0200

    Make route to add new movie to collection

--- Api/app.js
+++ Api/app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+
 let movies = JSON.parse(fs.readFileSync("./data/movies.json", "utf-8"));
 
 app.get("/api/movies", (req, res) => {
@@ -12,6 +14,31 @@ app.get("/api/movies", (req, res) => {
       movies,
     },
   });
+});
+
+app.post("/api/movies", (req, res) => {
+  const newId = movies[movies.length - 1].id + 1;
+
+  let newMove = Object.assign({ id: newId }, req.body);
+
+  movies.push(newMove);
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "Error writing to file",
+      });
+    }
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        movie: newMove,
+      },
+    });
+  });
+  // res.send("Movie added successfully");
 });
 
 app.listen(port, () => {
