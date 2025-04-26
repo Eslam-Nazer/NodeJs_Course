fe870f51edeaa9a00247fb2c8d8d88d6c34bca95
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-04-27 02:33:04 +0300

    Make confirm password is virtual to check password matching and crypt this password

--- Api/Model/UserModel.js
+++ Api/Model/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 let userSchema = mongoose.Schema({
   name: {
@@ -19,12 +20,43 @@ let userSchema = mongoose.Schema({
     required: [true, "Password is required"],
     trim: true,
     minlength: [8, "Password must be at least 8 characters long"],
+    validate: {
+      validator: function (value) {
+        return value === this._confirmPassword;
+      },
+      message: "Passwords and confirmPassword do not match",
+    },
   },
-  confirmPassword: {
-    type: String,
-    required: [true, "Confirm Password is required"],
-    trim: true,
-  },
+  // confirmPassword: {
+  //   type: String,
+  //   required: [true, "Confirm Password is required"],
+  //   trim: true,
+  //   validate: {
+  //     validator: function (value) {
+  //       return value === this.password;
+  //     },
+  //     message: "Passwords and confirmPassword do not match",
+  //   },
+  // },
+});
+
+userSchema
+  .virtual("confirmPassword")
+  .set(function (value) {
+    this._confirmPassword = value;
+  })
+  .get(function () {
+    console.log(this._confirmPassword);
+    return this._confirmPassword;
+  });
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  bcrypt.hash(this.password, 12).then((hashedPassword) => {
+    this.password = hashedPassword;
+    next();
+  });
 });
 
 const UserModel = mongoose.model("UserModel", userSchema);
